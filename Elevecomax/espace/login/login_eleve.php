ea26dfa7e3a5af17f2edd563b804e840d5239d9e
Iyed Amri <79877352+iyed-dev@users.noreply.github.com>
2023-03-10 13:11:41 +0100

    Add files via upload

---
+++ Elevecomax/espace/login/login_eleve.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="Iyed Amri"/>
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <title>Connexion</title>
+            <style>
+  body{
+    background: url(https://www.elegantthemes.com/blog/wp-content/uploads/2013/09/bg-12-full.jpg) no-repeat;
+  }
+  </style>
+        </head>
+        <body>
+        
+        <div class="login-form">
+             <?php 
+                if(isset($_GET['login_err']))
+                {
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte non existant
+                            </div>
+                        <?php
+                        break;
+                    }
+                }
+                ?> 
+            
+            <form action="connexion_eleve.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off" style="border-radius: 5px;">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off" style="border-radius: 5px;">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block" style="border-radius: 5px;">Connexion</button>
+                </div>   
+            </form>
+        </div>
+        <style>
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+</style>
+        </body>
+</html>
